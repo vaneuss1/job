016e5f4222e268142dd46f690f8f64896a99122a
vaneuss1 <issemenov99@mail.ru>
2022-06-03 01:16:00 +0300

    xml_refactor

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import xml.etree.ElementTree as ET
+from tqdm import tqdm
 
 
-def main(file_path):
-    root = ET.parse(file_path)
-    labels = root.find('meta').find('task').find('labels')
-
-    #search all attrubutes#
+def search_atr(file_path):
+    xml_file = ET.parse(file_path)
+    labels = xml_file.find('meta').find('task').find('labels')
     attributes = {}
     for label in labels:
         label_name = label.find('name').text
         attr_list = [attr.find('name').text for attr in label.find('attributes').findall('attribute')]
         list_of_tuple_att = zip(*[iter(attr_list)] * 2)
         attributes[label_name] = [attr_tuples for attr_tuples in list_of_tuple_att]
+    return attributes
 
-    #refactor meta labels#
-    for label in labels.findall('label'):
+
+def refactor_meta(file_path, attributes):
+    xml_file = ET.parse(file_path)
+    labels = xml_file.find('meta').find('task').find('labels')
+    for label in tqdm(labels.findall('label')):
         for atr in label.find('attributes'):
             for value in attributes.get(label.find('name').text):
                 if atr.find('name').text == 'Дополнительная' or atr.find('name').text == 'Надпись':
@@ -31,13 +34,20 @@ def main(file_path):
                     atr.find('name').text = '/'.join(value).lower().capitalize()
                     atr.find('default_value').text = value[0].lower()
 
-    #refactor attrubutes#
-    for image in root.findall('image'):
+    xml_file.write(f"{file_path.split('.')[0]}_new.xml", encoding='utf-8')
+    new_file = f"{file_path.split('.')[0]}_new.xml"
+    return new_file
+
+
+def refactor_attr(n_path):
+    xml_file = ET.parse(n_path)
+    for image in tqdm(xml_file.findall('image')):
         image_polygons = image.findall('polygon')
         for polygon in image_polygons:
-            for i in list(polygon):
-                if i.text == 'true':
-                    i.text = i.attrib['name'].lower()
+            for i in polygon.findall('attribute'):
+                text = i.text.strip()
+                if text == 'true':
+                    i.text = f"{i.attrib['name']}".lower()
                     for j in attributes[polygon.attrib['label']]:
                         if i.text == 'надпись':
                             i.attrib['name'] = 'Тип'
@@ -47,17 +57,17 @@ def main(file_path):
                             i.text = 'Логотип'
                         elif i.attrib['name'] in j:
                             i.attrib['name'] = '/'.join(j)
-                else:
+                elif text == 'false':
                     polygon.remove(i)
-            for attr in polygon.findall('attribute'):
-                if attr.text == 'true':
-                    attr.text = attr.attrib['name'].lower()
-    #rewrite
+    xml_file.write(n_path, encoding='utf-8')
+    new_file = n_path
+    return new_file
 
-    root.write(f"{file_path.split('.')[0]}+_new.xml", encoding='utf-8')
 
-    #Delete dubles#
-    for label in labels:
+def delete_dubles(n_path):
+    xml_file = ET.parse(n_path)
+    labels = xml_file.find('meta').find('task').find('labels')
+    for label in tqdm(labels):
         atrs = label.find('attributes')
         atr_list = atrs.findall('attribute')
         visited = []
@@ -66,16 +76,22 @@ def main(file_path):
                 atrs.remove(atr)
             else:
                 visited.append(atr.find('name').text)
+    xml_file.write(n_path, encoding='utf-8')
+    new_file = n_path
+    return new_file
 
-    root.write(f"{file_path.split('.')[0]}_new.xml", encoding='utf-8')
-    #short name#
-    images = root.findall('image')
-    for img in images:
+
+def short_name(n_path):
+    xml_file = ET.parse(n_path)
+    images = xml_file.findall('image')
+    for img in tqdm(images):
         img.attrib['name'] = img.attrib['name'].split('/')[-1]
-
-    root.write(f"{file_path.split('.')[0]}_new.xml", encoding='utf-8')
-
+    xml_file.write(n_path, encoding='utf-8')
 
 if __name__ == '__main__':
-    file_path = r'C:\Users\Семенов Иван\PycharmProjects\job\annotations.xml'
-    main(file_path)
+    path = r''
+    attributes = search_atr(path)
+    n_p = refactor_meta(path, attributes)
+    n_p = refactor_attr(n_p)
+    n_p = delete_dubles(n_p)
+    short_name(n_p)
